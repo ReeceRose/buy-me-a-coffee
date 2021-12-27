dc4814ad10bc52397a2df34d5799281f0ddcf060
Reece Rose <reecerose18@gmail.com>
2021-12-27 12:12:58 -0500

    Removed web project

--- web/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setupTests.js'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-};
