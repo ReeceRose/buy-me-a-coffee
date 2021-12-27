1133aee336285adbf2350496b52db15a7f3256d2
Reece Rose <reecerose18@gmail.com>
2021-12-27 12:21:35 -0500

    Cloned web from newer version of next.js-template repo

---
+++ web/pages/index.tsx
@@ -0,0 +1,12 @@
+import SEO from '../components/SEO';
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <SEO
+        title="Buy Me a Coffee"
+        description="Crypto payment version of buy me a coffee"
+      />
+    </div>
+  );
+}
