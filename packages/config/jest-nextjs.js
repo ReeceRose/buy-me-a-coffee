12900c229531d1d39b916c7ce81b8708446604d2
Reece Rose <reecerose18@gmail.com>
2022-02-07 21:46:53 -0500

    Cloned monorepo-template. Removed old code

---
+++ packages/config/jest-nextjs.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ...require('./jest-common'),
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  testEnvironment: 'jest-environment-jsdom',
+};
---
+++ packages/tsconfig/README.md
@@ -0,0 +1,3 @@
+# TS config package
+
+Common TypeScript configuration files shared across any app which uses TypeScript.
