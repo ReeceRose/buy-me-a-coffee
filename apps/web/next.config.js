1f7bc70d558ba435a8a340afc3b37b349eb9ff27
Reece Rose <reecerose18@gmail.com>
2022-02-17 16:49:50 -0500

    Package upgrades

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,6 +4,9 @@ const withTM = require('next-transpile-modules')(['ui']);
 module.exports = withTM({
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    outputStandalone: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
